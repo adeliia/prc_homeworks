5ad4fff26d26b4c1f1a4d2b91e072a3db63d0fa6
Adeliia Gataullina <adeliia.gataullina@gmail.com>
2019-10-14 13:58:24 +0000

    dec2hex mistakes correcting

--- First_task_dec2hex/main.cpp
+++ First_task_dec2hex/main.cpp
@@ -5,17 +5,30 @@ using namespace std;
 
 const char hexchar[17] = {"0123456789ABCDEF"};
 
-string dec2hez(int dec) {
+string dec2hex(int dec) {
     string hex;
     int rem;
+    bool negative;
+
+    if (dec < 0) {
+        negative = true;
+        dec = -dec;
+    }
+    else if (dec == 0) {
+        return "0";
+    }
     while (dec) {
-        hex += hexchar[dec%16];
+        hex = hexchar[dec%16] + hex;
+        dec = dec/16;
+    }
+    if (negative) {
+        hex = "-" + hex;
     }
     return hex;
 }
 
-void printHex(string what) {
-    cout << what << endl;
+void printHex(int dec, string hex) {
+    cout << "DEC " << dec << " = HEX " << hex << endl << endl;;
 }
 
 int main() {
@@ -23,12 +36,21 @@ int main() {
     int dec;
     string hex;
 
-    cout << "PLease, enter the decimal number:\n";
-    cin >> dec;
+    while(true) {
+        cout << "Please, enter the decimal number:\n";
+        cin >> dec;
 
-    hex = dec2hez(dec);
-    printHex(hex);
-
+        if (cin.eof()) break;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore();
+            cout << "Error. No interger was input.\n";
+        }
+        else {
+            hex = dec2hex(dec);
+            printHex(dec, hex);
+        }
+    }
 
     return 0;
-}
+}s
